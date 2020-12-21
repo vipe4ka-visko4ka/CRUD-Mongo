41c2b4d619df4a827132d1a12da76288f3d1cca3
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-12-21 17:21:12 +0300

    Category controller

---
+++ API/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using API.Data.Repositories;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController(ICategoryRepository repository, ILogger<CategoryController> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Category>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            var categories = await _repository.GetAll();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetCategory")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Category), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Category>> GetCategory(string id)
+        {
+            var category = await _repository.GetById(id);
+
+            if (category == null)
+            {
+                _logger.LogError($"Category with id: {id}, hasn't been found in database.");
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+    }
+}
