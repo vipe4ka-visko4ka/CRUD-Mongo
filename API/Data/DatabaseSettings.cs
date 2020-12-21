985f2bd21f706a84404aae9abcf089444eb3af55
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-12-21 14:16:35 +0300

    Database config

---
+++ API/Data/DatabaseSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Data
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+    }
+
+    public interface IDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+        string CategoryCollectionName { get; set; }
+        string ProductCollectionName { get; set; }
+    }
+}
