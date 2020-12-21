2e55bdb419de7d320425504f42f9a9708665feb5
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-12-21 16:47:02 +0300

    Category repository

---
+++ API/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using API.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly IDatabaseContext _context;
+
+        public CategoryRepository(IDatabaseContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<Category>> GetAll()
+        {
+            return await _context
+                .Categories
+                .Find(c => true)
+                .ToListAsync();
+        }
+
+        public async Task<Category> GetById(string id)
+        {
+            return await _context
+                .Categories
+                .Find(c => c.Id == id)
+                .FirstOrDefaultAsync();
+        }
+    }
+
+    public interface ICategoryRepository
+    {
+        public Task<IEnumerable<Category>> GetAll();
+        public Task<Category> GetById(string id);
+    }
+}
